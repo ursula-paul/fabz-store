4779324f1ae7e48f3845b0ebaf165a8a432e2c89
ursula-paul <ursula.paul@decagon.dev>
2022-03-29 00:35:05 +0100

    moving directory-items and category-items into seperate components

--- src/App.js
+++ src/App.js
@@ -1,48 +1,45 @@
+import DirectoryItem from './componets/directory-items/diretory-componet';
 
 
-const  App =() => {
+import './categories.styles.scss'
 
-  const categories =[
+
+const App = () => {
+  const categories = [
     {
-      id:1,
-      title:'Hats',
+      id: 1,
+      title: 'hats',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
     },
-
     {
-      id:2,
-      title:'Jackets',
+      id: 2,
+      title: 'jackets',
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
     },
-
     {
-      id:3,
-      title:'Sneakers',
+      id: 3,
+      title: 'sneakers',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
     },
-
     {
-      id:4,
-      title:'Ladies',
+      id: 4,
+      title: 'womens',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
     },
-
     {
-      id:5,
-      title:'Gents',
-    }
-  ]
-  return ( 
-    <div className="categories-container">
+      id: 5,
+      title: 'mens',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+    },
+  ];
 
-      {categories.map(({title}) => (
-         <div className="category-container">
-        <div className="background-image" />
-        <div className="category-body-container"> 
-        <h2>{title}</h2>
-        <p>Shop Now</p>
-        </div>
-      </div>
-      ))}
-      
-    </div>
-  );
-}
+  return <DirectoryItem categories={categories} />;
+};
+
+
+
+
+
+
 
 export default App;
---
+++ src/componets/directory-items/diretory-componet.jsx
@@ -0,0 +1,15 @@
+import CategoryItem from "../category-item/category-item-componet";
+
+import "./directory-styles.scss";
+
+const DirectoryItem = ({ categories }) => {
+	return (
+		<div className="directory-container">
+			{categories.map((category) => (
+				<CategoryItem key={category.id} category={category} />
+			))}
+		</div>
+	);
+};
+
+export default DirectoryItem;
