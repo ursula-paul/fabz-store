f91e22fb8e9e152fca89a61e3de82fe485dfe867
ursula-paul <ursula.paul@decagon.dev>
2022-03-29 02:50:51 +0100

    added react-router-dom and performed basic routing

--- src/App.js
+++ src/App.js
@@ -1,45 +1,24 @@
-import DirectoryItem from './componets/directory-items/diretory-componet';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Home from './routes/home-page/home-component';
 
-
-import './categories.styles.scss'
-
-
-const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-    },
-    {
-      id: 2,
-      title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-    },
-    {
-      id: 3,
-      title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-    },
-    {
-      id: 4,
-      title: 'ladies',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-    },
-    {
-      id: 5,
-      title: 'gents',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-    },
-  ];
-
-  return <DirectoryItem categories={categories} />;
+const Shop =()=>{
+  return(
+    <h1>I am a shop</h1>
+  )
+  
 };
 
+const App =()=> {
+  return (
+    <Routes>
+  
+      <Route path='/'  element={<Home />}>
+      <Route path='shop' element={<Shop />} />
 
-
-
-
-
+      </Route>
+    </Routes>
+  );
+}
 
 export default App;
